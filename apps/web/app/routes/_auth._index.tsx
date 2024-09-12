5a5353e328b2771db12dffb369cb8899f1b6a3a3
David Hollins <davidhollins06@gmail.com>
2024-09-12 21:19:06 +0100

    Finishing the initial dashboard ui

--- apps/web/app/routes/_auth._index.tsx
+++ apps/web/app/routes/_auth._index.tsx
@@ -4,9 +4,9 @@ import styles from "../styles/global.css?url";
 import { requireAuthSession } from "../modules/auth/session.server";
 import { getOwnedRooms } from "@musicall/api/room";
 import { Form, Link, useLoaderData } from "@remix-run/react";
-import { ActionIcon, Box, Button, Card, Group, SimpleGrid, Space, Stack, Title, Tooltip } from "@mantine/core";
+import { ActionIcon, Box, Button, Card, Group, SimpleGrid, Space, Stack, Text, Title, Tooltip } from "@mantine/core";
 import { useSessionStore } from "../store/sessionStore";
-import { IconCopy, IconDoorEnter } from "@tabler/icons-react";
+import { IconCopy, IconDoor, IconDoorEnter, IconPlus, IconUserPlus } from "@tabler/icons-react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const { userId } = await requireAuthSession(request);
@@ -24,22 +24,20 @@ export default function Index() {
 
     return (
         <Stack h="100%" p={32}>
-            <Box>
-                <Title>Hello {user?.name}</Title>
-                Your rooms:
-            </Box>
-            <SimpleGrid
-                flex={1}
-                cols={3}
-                style={{
-                    gridTemplateRows: "1fr 1fr",
-                }}
-            >
+            <Group flex={1} gap={12} align="start">
                 {rooms.map((room) => (
-                    <Card withBorder key={room.id}>
+                    <Card w="300px" h="300px" withBorder shadow="md" radius="md" key={room.id}>
                         <Stack h="100%" justify="space-between">
-                            <Title order={2}>{room.name}</Title>
+                            <Box>
+                                <Title order={2}>{room.name}</Title>
+                                <Text>Owner: You</Text>
+                            </Box>
                             <Group justify="flex-end">
+                                <Tooltip label="Invite Friends">
+                                    <ActionIcon variant="default" size="lg">
+                                        <IconUserPlus />
+                                    </ActionIcon>
+                                </Tooltip>
                                 <Tooltip label="Copy link">
                                     <ActionIcon variant="default" size="lg">
                                         <IconCopy />
@@ -52,14 +50,19 @@ export default function Index() {
                                         variant="default"
                                         size="lg"
                                     >
-                                        <IconDoorEnter />
+                                        <IconDoor />
                                     </ActionIcon>
                                 </Tooltip>
                             </Group>
                         </Stack>
                     </Card>
                 ))}
-            </SimpleGrid>
+                <Card w="300px" h="300px" shadow="md" radius="md" withBorder p={0}>
+                    <ActionIcon onClick={() => {}} style={{ border: 0 }} variant="default" h="100%" w="100%">
+                        <IconPlus size={100} />
+                    </ActionIcon>
+                </Card>
+            </Group>
         </Stack>
     );
 }
