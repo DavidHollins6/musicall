d05ad7b39ebb7894ef40c86ded993a1743cdb8d7
David Hollins <davidhollins06@gmail.com>
2025-02-14 14:27:43 +0000

    Simplifying things

--- apps/web/app/routes/_auth._index.tsx
+++ apps/web/app/routes/_auth._index.tsx
@@ -3,10 +3,10 @@ import { type LinksFunction, type LoaderFunctionArgs, json } from "@remix-run/no
 import styles from "../styles/global.css?url";
 import { requireAuthSession } from "../modules/auth/session.server";
 import { getOwnedRooms } from "@musicall/api/room";
-import { Form, Link, useLoaderData } from "@remix-run/react";
-import { ActionIcon, Box, Button, Card, Group, SimpleGrid, Space, Stack, Text, Title, Tooltip } from "@mantine/core";
+import { Link, useLoaderData } from "@remix-run/react";
+import { ActionIcon, Box, Card, Center, Group, Stack, Title, Tooltip } from "@mantine/core";
 import { useSessionStore } from "../store/sessionStore";
-import { IconCopy, IconDoor, IconDoorEnter, IconPlus, IconUserPlus } from "@tabler/icons-react";
+import { IconCopy, IconDoor } from "@tabler/icons-react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const { userId } = await requireAuthSession(request);
@@ -23,46 +23,40 @@ export default function Index() {
     const { rooms } = useLoaderData<typeof loader>();
 
     return (
-        <Stack h="100%" p={32}>
-            <Group flex={1} gap={12} align="start">
-                {rooms.map((room) => (
-                    <Card w="300px" h="300px" withBorder shadow="md" radius="md" key={room.id}>
-                        <Stack h="100%" justify="space-between">
+        <Center pos="relative">
+            {rooms.map((room) => (
+                <Card w="300px" h="300px" withBorder shadow="md" radius="50%" key={room.id}>
+                    <Center h="100%">
+                        <Stack>
                             <Box>
-                                <Title order={2}>{room.name}</Title>
-                                <Text>Owner: You</Text>
+                                <Title order={2}>{user?.name}&#39;s Room</Title>
                             </Box>
-                            <Group justify="flex-end">
-                                <Tooltip label="Invite Friends">
-                                    <ActionIcon variant="default" size="lg">
-                                        <IconUserPlus />
-                                    </ActionIcon>
-                                </Tooltip>
-                                <Tooltip label="Copy link">
-                                    <ActionIcon variant="default" size="lg">
-                                        <IconCopy />
-                                    </ActionIcon>
-                                </Tooltip>
-                                <Tooltip label="Enter room">
-                                    <ActionIcon
-                                        component={Link}
-                                        to={`/call?roomId=${room.id}`}
-                                        variant="default"
-                                        size="lg"
-                                    >
-                                        <IconDoor />
-                                    </ActionIcon>
-                                </Tooltip>
-                            </Group>
+                            <Center>
+                                <Group justify="flex-end">
+                                    <Tooltip label="Copy link">
+                                        <ActionIcon variant="default" size="xl">
+                                            <IconCopy />
+                                        </ActionIcon>
+                                    </Tooltip>
+                                    <Tooltip label="Enter room">
+                                        <ActionIcon
+                                            component={Link}
+                                            to={`/call?roomId=${room.id}`}
+                                            variant="default"
+                                            size="xl"
+                                        >
+                                            <IconDoor />
+                                        </ActionIcon>
+                                    </Tooltip>
+                                </Group>
+                            </Center>
                         </Stack>
-                    </Card>
-                ))}
-                <Card w="300px" h="300px" shadow="md" radius="md" withBorder p={0}>
-                    <ActionIcon onClick={() => {}} style={{ border: 0 }} variant="default" h="100%" w="100%">
-                        <IconPlus size={100} />
-                    </ActionIcon>
+                    </Center>
                 </Card>
-            </Group>
-        </Stack>
+            ))}
+            <Title order={1} top="20px" left="20px" pos="absolute">
+                Welcome to Musicall
+            </Title>
+        </Center>
     );
 }
